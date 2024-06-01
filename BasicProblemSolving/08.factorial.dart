43ffb9b61539ad957d5997a41740ad6740d8029e
dev-asib <mail.asib.com@gmail.com>
2024-06-01 22:08:36 +0600

    Updated

---
+++ BasicProblemSolving/08.factorial.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+void main(){
+  stdout.write('Enter any integer number = ');
+  int? number = int.tryParse(stdin.readLineSync()!);
+  if(number is int){
+    if(number>=0){
+      int factorialResult = 1;
+      if(number>0){
+        for(int i = number; i>=1; i--){
+          factorialResult = factorialResult * i;
+        }
+        print("Factorial $number! = $factorialResult");
+      } else if(number==0){
+        factorialResult=1;
+        print("Factorial $number! = $factorialResult");
+      }
+
+    } else{
+      print("The number should be greater than or equal to 0");
+    }
+  } else{
+    print("Enter a valid number");
+  }
+}
